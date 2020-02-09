062cb8ec8b04e1d893a3d55c236e72dd71fb2638
Robson Mathias <robsond.mathias@gmail.com>
2020-02-09 15:02:01 -0300

    change config overrides

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 module.exports = (config, env) => {
-    config.output.filename = 'static/js/[name].js';
-    config.output.chunkFilename = 'static/js/[name].js';
+    config.output.filename = 'scripts/[name].js';
+    config.output.chunkFilename = 'scripts/[name].js';
     config.output.libraryTarget = 'amd';
     return config
 };
