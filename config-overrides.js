3b310bc3e40cc7638eea60c01c092aa47d3e24d2
Robson Mathias <robsond.mathias@gmail.com>
2020-02-09 14:47:34 -0300

    Change to umd script

--- config-overrides.js
+++ config-overrides.js
@@ -3,6 +3,6 @@ const path = require('path');
 module.exports = (config, env) => {
     config.output.filename = 'static/js/[name].js';
     config.output.chunkFilename = 'static/js/[name].js';
-    config.output.libraryTarget = 'system';
+    config.output.libraryTarget = 'umd';
     return config
 };
