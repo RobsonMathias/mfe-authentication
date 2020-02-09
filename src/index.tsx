cd8afdd1fcce53db3db1ab8af6616f987471808e
Robson Mathias <robsond.mathias@gmail.com>
2020-02-09 08:51:14 -0300

    Adding structure

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import singleSpaReact from 'single-spa-react';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './App';
+
+const reactLifecycles = singleSpaReact({
+    React,
+    ReactDOM,
+    rootComponent: App,
+});
+
+export const bootstrap = [reactLifecycles.bootstrap];
+
+export const mount = [reactLifecycles.mount];
+
+export const unmount = [reactLifecycles.unmount];
