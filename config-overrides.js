3d35f22d874569f4ed09f153068ef20a88ccdf48
Robson Mathias <robsond.mathias@gmail.com>
2020-02-09 11:41:24 -0300

    Test

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,10 @@
+const path = require('path');
+
 module.exports = (config, env) => {
+    config.entry = {
+        'main': './src/index.tsx'
+    };
+    config.resolve.extensions = ['.tsx', '.ts', '.js'];
     config.output.filename = 'static/js/[name].js';
     config.output.chunkFilename = 'static/js/[name].js';
     return config
