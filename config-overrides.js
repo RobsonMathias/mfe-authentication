39a52b927e4d65c48f29489995a3fd91b2db10d5
Robson Mathias <robsond.mathias@gmail.com>
2020-02-09 12:13:06 -0300

    Change librayrTarget

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,8 @@
 const path = require('path');
 
 module.exports = (config, env) => {
-    config.entry = {
-        'main': './src/index.tsx'
-    };
-    config.resolve.extensions = ['.tsx', '.ts', '.js'];
     config.output.filename = 'static/js/[name].js';
     config.output.chunkFilename = 'static/js/[name].js';
-    config.output.libraryTarget = 'umd';
+    config.output.libraryTarget = 'system';
     return config
 };
