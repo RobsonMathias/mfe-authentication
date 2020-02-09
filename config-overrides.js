a7b0a906e507271b2e25731f1afd44bb9d613c75
Robson Mathias <robsond.mathias@gmail.com>
2020-02-09 12:09:50 -0300

    Change build

--- config-overrides.js
+++ config-overrides.js
@@ -7,5 +7,6 @@ module.exports = (config, env) => {
     config.resolve.extensions = ['.tsx', '.ts', '.js'];
     config.output.filename = 'static/js/[name].js';
     config.output.chunkFilename = 'static/js/[name].js';
+    config.output.libraryTarget = 'umd';
     return config
 };
