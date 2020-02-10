2058f78dd6f59223770333af22dd0c05b4059977
Robson Mathias <robsond.mathias@gmail.com>
2020-02-10 09:42:01 -0300

    Adding new script

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,14 @@
 const path = require('path');
 
 module.exports = (config, env) => {
+    config.output.libraryTarget = 'amd';
     config.output.filename = 'scripts/[name].js';
     config.output.chunkFilename = 'scripts/[name].js';
-    config.output.libraryTarget = 'amd';
+    config.optimization.splitChunks = {
+        cacheGroups: {
+            default: false
+        }
+    };
+    config.optimization.runtimeChunk = false;
     return config
 };
