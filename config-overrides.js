e71132779b1926e040c2cd29627e1926db72c926
Robson Mathias <robsond.mathias@gmail.com>
2020-02-10 13:30:27 -0300

    Adding new release as test

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,33 @@
-module.exports = (config) => {
-    config.output.libraryTarget = 'amd';
-    config.output.filename = 'scripts/[name].js';
-    config.output.chunkFilename = 'scripts/[name].js';
-    config.optimization.splitChunks = {
-        cacheGroups: {
-            default: false
-        }
-    };
-    config.optimization.runtimeChunk = false;
-    return config
+const { override } = require('customize-cra');
+
+module.exports = {
+    webpack: override(
+        (config) => {
+            config.output.libraryTarget = 'amd';
+            config.output.filename = 'scripts/[name].js';
+            config.output.chunkFilename = 'scripts/[name].js';
+            config.optimization.splitChunks = {
+                cacheGroups: {
+                    default: false
+                }
+            };
+            config.optimization.runtimeChunk = false;
+            return config;
+        },
+    ),
+
+    jest: config => {
+        return config;
+    },
+
+    devServer: configFunction => (proxy, allowedHost) => {
+        const config = configFunction(proxy, allowedHost);
+        config.disableHostCheck = true;
+        config.headers = { 'Access-Control-Allow-Origin': '*' };
+        return config;
+    },
+
+    paths: (paths, env) => {
+        return paths;
+    }
 };
