529d2242e1168c2e1c85e1c5d87d780f0f1d5c9d
Robson Mathias <robsond.mathias@gmail.com>
2020-02-11 07:36:51 -0300

    Adding validate js and start a composition of form events page

--- src/containers/sign-in/SignInContainer.tsx
+++ src/containers/sign-in/SignInContainer.tsx
@@ -1,14 +1,54 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import InputFieldComponent from '../../components/input-field';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {signIn} from '../../stores/sign-in/SignInAction';
+import validate from 'validate.js';
 
 interface SignInContainerProps {
     signIn: typeof signIn
 }
 
 class SignInContainer extends React.Component<SignInContainerProps> {
+
+    public formState: any;
+    public setFormState: any;
+    private schema = {
+        email: {
+            presence: { allowEmpty: false, message: 'is required' },
+            email: true,
+            length: {
+                maximum: 64
+            }
+        },
+        password: {
+            presence: { allowEmpty: false, message: 'is required' },
+            length: {
+                maximum: 128
+            }
+        }
+    };
+
+    constructor(props: Readonly<SignInContainerProps>) {
+        super(props);
+        const [formState, setFormState] = useState({
+            isValid: false,
+            values: {},
+            touched: {},
+            errors: {}
+        });
+        this.formState = formState;
+        this.setFormState = setFormState;
+
+        useEffect(() => {
+            const errors = validate(formState.values, this.schema);
+            setFormState(formState => ({
+                ...formState,
+                isValid: !errors,
+                errors: errors || {}
+            }));
+        }, [formState.values]);
+    }
 
     signIn() {
         // this.props.signIn
