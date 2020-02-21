c19fec949109b7550b9796bb0f0621c416574b75
Robson Mathias <robsond.mathias@gmail.com>
2020-02-21 12:05:58 -0300

    Fixing css load

--- src/pages/app/App.tsx
+++ src/pages/app/App.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import './App.css';
 import {Provider} from 'react-redux';
 import {store} from '../../stores/store.config';
 import LoginPage from '../login';
 import ForgotPasswordPage from '../forgot-password';
+// @ts-ignore
+import styles from  './App.css';
 
 
 class App extends React.Component {
+
     componentDidMount() {
     }
 
     render() {
+        useEffect(() => {
+            styles.use();
+            return () => {
+                styles.unuse();
+            };
+        }, []);
+
         return (
             <div className="AuthenticationApp">
                 <Provider store={store}>
